d22828a57e7fa8ec35d2b712c1cdffcd9e96b52c
Abdurrahman Hac Said <92548247+ABDURRAHMAN4498@users.noreply.github.com>
2024-02-19 09:04:31 +0300

    A

--- Program.cs
+++ Program.cs
@@ -8,10 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
-//});
+builder.Services.AddSwaggerGen(c =>
+{
+   c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,8 +21,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+    app.UseSwagger();
+    app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
